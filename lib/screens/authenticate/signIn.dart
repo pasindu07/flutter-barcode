f4b71e2d24231e59705627624c2289d0e48da238
Pasindu Dissanayake <50499112+pasindu07@users.noreply.github.com>
2021-04-26 18:43:35 +0530

    done front

--- lib/screens/authenticate/signIn.dart
+++ lib/screens/authenticate/signIn.dart
@@ -1,6 +1,6 @@
 import 'package:barcode/services/auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:barcode/shared/constant.dart';
 
 
 
@@ -16,14 +16,56 @@ class _SignInState extends State<SignIn> {
 
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+
   bool loading = false;
+  
+  //text field var
+  String email = '';
+  String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.green[50],
       appBar: AppBar(
         backgroundColor: Colors.blue[700],
         title: Text('Bar Code Reader'),
+      ),
+      body: Container(
+        margin: EdgeInsets.fromLTRB(0.0, 200.0, 0.0, 0.0),
+        padding: EdgeInsets.symmetric(horizontal:50.0,vertical:20.0),
+        child: Form(
+          key: _formKey,
+          child:Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: textInputDecoration.copyWith(hintText:'Email'),
+                validator: (value) => value.isEmpty?'Enter an Email':null,
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+              SizedBox(height:20.0),
+              TextFormField(
+                decoration: textInputDecoration.copyWith(hintText:'Password'),
+                validator: (value) => value.isEmpty?'Enter The Password':null,
+                onChanged: (value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+              ),
+              SizedBox(height:20.0),
+              ElevatedButton.icon(
+                onPressed: (){},
+                icon: Icon(Icons.login),
+                label: Text('Sign In'), 
+              ),
+          ],),
+        ),
       ),
     );
   }
