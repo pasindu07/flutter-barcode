1d90079040e4e5e8b440b8e2d2be6c0441680166
Pasindu Dissanayake <50499112+pasindu07@users.noreply.github.com>
2021-04-26 23:01:54 +0530

    back done

--- lib/screens/authenticate/signIn.dart
+++ lib/screens/authenticate/signIn.dart
@@ -1,4 +1,5 @@
 import 'package:barcode/services/auth.dart';
+import 'package:barcode/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:barcode/shared/constant.dart';
 
@@ -23,73 +24,99 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green[50],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[700],
-        title: Text('Bar Code Reader'),
-      ),
-      body: Wrap(
-        children: [
-          Container(
-            margin: EdgeInsets.fromLTRB(0.0, 250.0, 0.0, 0.0),
-            padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                    validator: (value) =>
-                        value.isEmpty ? 'Enter an Email' : null,
-                    onChanged: (value) {
-                      setState(() {
-                        email = value;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    obscureText: true,
-                    decoration:
-                        textInputDecoration.copyWith(hintText: 'Password'),
-                    validator: (value) =>
-                        value.isEmpty ? 'Enter The Password' : null,
-                    onChanged: (value) {
-                      setState(() {
-                        password = value;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.login_rounded),
-                    label: Text('Sign In'),
-                  ),
-                ],
-              ),
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.green[50],
+            appBar: AppBar(
+              backgroundColor: Colors.blue[700],
+              title: Text('Bar Code Reader'),
             ),
-          ),
-          Center(
-            child: Container(
-              margin: EdgeInsets.only(top: 130.0),
-              padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 50.0),
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  widget.toggleView();
-                },
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.green[400]),
+            body: Wrap(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(0.0, 250.0, 0.0, 0.0),
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration:
+                              textInputDecoration.copyWith(hintText: 'Email'),
+                          validator: (value) =>
+                              value.isEmpty ? 'Enter an Email' : null,
+                          onChanged: (value) {
+                            setState(() {
+                              email = value;
+                            });
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: textInputDecoration.copyWith(
+                              hintText: 'Password'),
+                          validator: (value) =>
+                              value.isEmpty ? 'Enter The Password' : null,
+                          onChanged: (value) {
+                            setState(() {
+                              password = value;
+                            });
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        ElevatedButton.icon(
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              setState(() {
+                                loading = true;
+                              });
+                            }
+
+                            dynamic result = await _auth
+                                .logInWithEmailAndPassword(email, password);
+
+                            if (result == null) {
+                              setState(() {
+                                error = 'Sign In Failed';
+                                loading = false;
+                              });
+                            }
+                          },
+                          icon: Icon(Icons.login_rounded),
+                          label: Text('Sign In'),
+                        ),
+                        SizedBox(height: 12.0),
+                        Text(
+                          error,
+                          style: TextStyle(color: Colors.red, fontSize: 14.0),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-                icon: Icon(Icons.app_registration),
-                label: Text('Not a Member?'),
-              ),
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.only(top: 110.0),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 50.0, vertical: 50.0),
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        widget.toggleView();
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.green[400]),
+                      ),
+                      icon: Icon(Icons.app_registration),
+                      label: Text('Not a Member?'),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-    );
+          );
   }
 }
